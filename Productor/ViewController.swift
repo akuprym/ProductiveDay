81808ef76a664840cad88fd364d8d33f2c093613
Anna Kuprym <annakupriyanchik99@gmail.com>
2022-11-27 00:45:28 +0300

    Initial Commit

---
+++ Productor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Productor
+//
+//  Created by admin on 27.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
