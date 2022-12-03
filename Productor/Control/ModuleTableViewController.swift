3290fcfaf9f474fe8883389171ab2ad433e5270e
Anna Kuprym <annakupriyanchik99@gmail.com>
2022-12-04 01:34:40 +0300

    Data Model created

--- Productor/Control/ModuleTableViewController.swift
+++ Productor/Control/ModuleTableViewController.swift
@@ -8,13 +8,27 @@
 import UIKit
 
 class ModuleTableViewController: UITableViewController {
+    
+    var modules = [Module]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return modules.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ModuleCell", for: indexPath)
+        cell.textLabel?.text = modules[indexPath.row].title
+        return cell
+    }
+    
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
     }
     
     
